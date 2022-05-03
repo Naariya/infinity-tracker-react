3743f2a574990609ee996e5a3d89cbc854910934
Nannapat Ariyakornwijit <nannapat9858@gmail.com>
2022-05-03 13:56:28 -0500

    remove link

--- src/Api/index.jsx
+++ src/Api/index.jsx
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 export const client = Axios.create({
-    baseURL: 'https://infinity-tracker-backend.vercel.app',
+    baseURL: 'https://infinity-tracker-backend.vercel.app/',
     validateStatus: (status) => {
         return status >= 200 && status <300;
     }
--- src/Components/ActivitiesForm/ActivitiesForm.jsx
+++ src/Components/ActivitiesForm/ActivitiesForm.jsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { useState } from 'react';
 import axios from 'axios';
 import './ActivitiesForm.css';
 import Button from '../Button/Button';
